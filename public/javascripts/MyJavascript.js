fbae1d798767301e484df4412f83eff5ea864f0e
Vo Thanh Dien <vtdien96@gmail.com>
2017-10-19 01:02:30 +0700

    commit. thay doi giao dien vung nguoi dung

---
+++ public/javascripts/MyJavascript.js
@@ -0,0 +1,10 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 10.7626391, lng: 106.6820268},
+        zoom: 16
+    });
+}
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,20 @@ var router = express.Router();
 const https = require("https");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data;
-  https.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ3eH0BhwvdTERPZpT1PEAOQQ&' +
-      'key=AIzaSyCqG55x6-7BVevi2doMzzPFqmmATL55iPU',function (res) {
-
-      res.on('data',function (chunk) {
-          data += chunk;
-      });
-      res.on('end',function () {
-          console.log(data);
-      });
-  }).on('error',function (err) {
-      console.log("Error: " + err.message);
-  })
-  res.render('index', { title: data });
+  // var data = '';
+  // https.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ3eH0BhwvdTERPZpT1PEAOQQ&' +
+  //     'key=AIzaSyCqG55x6-7BVevi2doMzzPFqmmATL55iPU',function (res) {
+  //
+  //     res.on('data',function (chunk) {
+  //         data += chunk;
+  //     });
+  //     res.on('end',function () {
+  //         console.log(data);
+  //     });
+  // }).on('error',function (err) {
+  //     console.log("Error: " + err.message);
+  // });
+  res.render('index', { title: 'hello' });
 });
 
 module.exports = router;
